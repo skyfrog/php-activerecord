a37fa86e9cf79c35f8bd673fb5441b22c5e8c20d
Vadim Manikhin <vadim.manikhin@gmail.com>
2022-11-09 15:38:44 +0400

    #PD-732: php81 compat.

--- lib/DateTime.php
+++ lib/DateTime.php
@@ -33,172 +33,216 @@ namespace ActiveRecord;
  * @package ActiveRecord
  * @see http://php.net/manual/en/class.datetime.php
  */
-class DateTime extends \DateTime implements DateTimeInterface
+class DateTime extends \DateTime implements \ActiveRecord\DateTimeInterface
 {
-	/**
-	 * Default format used for format() and __toString()
-	 */
-	public static $DEFAULT_FORMAT = 'rfc2822';
+    /**
+     * Default format used for format() and __toString()
+     */
+    public static $DEFAULT_FORMAT = 'rfc2822';
 
-	/**
-	 * Pre-defined format strings.
-	 */
-	public static $FORMATS = array(
-		'db'      => 'Y-m-d H:i:s',
-		'number'  => 'YmdHis',
-		'time'    => 'H:i',
-		'short'   => 'd M H:i',
-		'long'    => 'F d, Y H:i',
-		'atom'    => \DateTime::ATOM,
-		'cookie'  => \DateTime::COOKIE,
-		'iso8601' => \DateTime::ISO8601,
-		'rfc822'  => \DateTime::RFC822,
-		'rfc850'  => \DateTime::RFC850,
-		'rfc1036' => \DateTime::RFC1036,
-		'rfc1123' => \DateTime::RFC1123,
-		'rfc2822' => \DateTime::RFC2822,
-		'rfc3339' => \DateTime::RFC3339,
-		'rss'     => \DateTime::RSS,
-		'w3c'     => \DateTime::W3C);
+    /**
+     * Pre-defined format strings.
+     */
+    public static $FORMATS = array(
+        'db'      => 'Y-m-d H:i:s',
+        'number'  => 'YmdHis',
+        'time'    => 'H:i',
+        'short'   => 'd M H:i',
+        'long'    => 'F d, Y H:i',
+        'atom'    => \DateTime::ATOM,
+        'cookie'  => \DateTime::COOKIE,
+        'iso8601' => \DateTime::ISO8601,
+        'rfc822'  => \DateTime::RFC822,
+        'rfc850'  => \DateTime::RFC850,
+        'rfc1036' => \DateTime::RFC1036,
+        'rfc1123' => \DateTime::RFC1123,
+        'rfc2822' => \DateTime::RFC2822,
+        'rfc3339' => \DateTime::RFC3339,
+        'rss'     => \DateTime::RSS,
+        'w3c'     => \DateTime::W3C);
 
-	private $model;
-	private $attribute_name;
+    private $model;
+    private $attribute_name;
 
-	public function attribute_of($model, $attribute_name)
-	{
-		$this->model = $model;
-		$this->attribute_name = $attribute_name;
-	}
+    public function attribute_of($model, $attribute_name)
+    {
+        $this->model = $model;
+        $this->attribute_name = $attribute_name;
+    }
 
-	/**
-	 * Formats the DateTime to the specified format.
-	 *
-	 * <code>
-	 * $datetime->format();         # uses the format defined in DateTime::$DEFAULT_FORMAT
-	 * $datetime->format('short');  # d M H:i
-	 * $datetime->format('Y-m-d');  # Y-m-d
-	 * </code>
-	 *
-	 * @see FORMATS
-	 * @see get_format
-	 * @param string $format A format string accepted by get_format()
-	 * @return string formatted date and time string
-	 */
-	public function format($format=null)
-	{
-		return parent::format(self::get_format($format));
-	}
+    /**
+     * Formats the DateTime to the specified format.
+     *
+     * <code>
+     * $datetime->format();         # uses the format defined in DateTime::$DEFAULT_FORMAT
+     * $datetime->format('short');  # d M H:i
+     * $datetime->format('Y-m-d');  # Y-m-d
+     * </code>
+     *
+     * @see FORMATS
+     * @see get_format
+     * @param string $format A format string accepted by get_format()
+     * @return string formatted date and time string
+     */
+    public function format(string $format=null): string
+    {
+        return parent::format(self::get_format($format));
+    }
 
-	/**
-	 * Returns the format string.
-	 *
-	 * If $format is a pre-defined format in $FORMATS it will return that otherwise
-	 * it will assume $format is a format string itself.
-	 *
-	 * @see FORMATS
-	 * @param string $format A pre-defined string format or a raw format string
-	 * @return string a format string
-	 */
-	public static function get_format($format=null)
-	{
-		// use default format if no format specified
-		if (!$format)
-			$format = self::$DEFAULT_FORMAT;
+    /**
+     * Returns the format string.
+     *
+     * If $format is a pre-defined format in $FORMATS it will return that otherwise
+     * it will assume $format is a format string itself.
+     *
+     * @see FORMATS
+     * @param string $format A pre-defined string format or a raw format string
+     * @return string a format string
+     */
+    public static function get_format($format=null)
+    {
+        // use default format if no format specified
+        if (!$format)
+            $format = self::$DEFAULT_FORMAT;
 
-		// format is a friendly
-		if (array_key_exists($format, self::$FORMATS))
-			 return self::$FORMATS[$format];
+        // format is a friendly
+        if (array_key_exists($format, self::$FORMATS))
+            return self::$FORMATS[$format];
 
-		// raw format
-		return $format;
-	}
+        // raw format
+        return $format;
+    }
 
-	/**
-	 * This needs to be overriden so it returns an instance of this class instead of PHP's \DateTime.
-	 * See http://php.net/manual/en/datetime.createfromformat.php
-	 */
-	public static function createFromFormat($format, $time, $tz = null)
-	{
-		$phpDate = $tz ? parent::createFromFormat($format, $time, $tz) : parent::createFromFormat($format, $time);
-		if (!$phpDate)
-			return false;
-		// convert to this class using the timestamp
-		$ourDate = new static(null, $phpDate->getTimezone());
-		$ourDate->setTimestamp($phpDate->getTimestamp());
-		return $ourDate;
-	}
+    /**
+     * This needs to be overriden so it returns an instance of this class instead of PHP's \DateTime.
+     * See http://php.net/manual/en/datetime.createfromformat.php
+     * @param string $format
+     * @param string $time
+     * @param null $tz
+     * @return DateTime|bool|\DateTime
+     * @throws \Exception
+     */
+    public static function createFromFormat(string $format, string $time, ?\DateTimeZone $tz = null): \DateTime
+    {
+        $phpDate = $tz ? parent::createFromFormat($format, $time, $tz) : parent::createFromFormat($format, $time);
+        if (!$phpDate)
+            return false;
+        // convert to this class using the timestamp
+        $ourDate = new static(null, $phpDate->getTimezone());
+        $ourDate->setTimestamp($phpDate->getTimestamp());
+        return $ourDate;
+    }
 
-	public function __toString()
-	{
-		return $this->format();
-	}
+    public function __toString()
+    {
+        return $this->format();
+    }
 
-	/**
-	 * Handle PHP object `clone`.
-	 *
-	 * This makes sure that the object doesn't still flag an attached model as
-	 * dirty after cloning the DateTime object and making modifications to it.
-	 *
-	 * @return void
-	 */
-	public function __clone()
-	{
-		$this->model = null;
-		$this->attribute_name = null;
-	}
+    /**
+     * Handle PHP object `clone`.
+     *
+     * This makes sure that the object doesn't still flag an attached model as
+     * dirty after cloning the DateTime object and making modifications to it.
+     *
+     * @return void
+     */
+    public function __clone()
+    {
+        $this->model = null;
+        $this->attribute_name = null;
+    }
 
-	private function flag_dirty()
-	{
-		if ($this->model)
-			$this->model->flag_dirty($this->attribute_name);
-	}
+    private function flag_dirty()
+    {
+        if ($this->model)
+            $this->model->flag_dirty($this->attribute_name);
+    }
 
-	public function setDate($year, $month, $day)
-	{
-		$this->flag_dirty();
-		return parent::setDate($year, $month, $day);
-	}
+    /**
+     * @param int $year
+     * @param int $month
+     * @param int $day
+     * @return \DateTime
+     */
+    public function setDate(int $year, int $month, int $day): \DateTime
+    {
+        $this->flag_dirty();
+        return parent::setDate($year, $month, $day);
+    }
 
-	public function setISODate($year, $week , $day = 1)
-	{
-		$this->flag_dirty();
-		return parent::setISODate($year, $week, $day);
-	}
+    /**
+     * @param int $year
+     * @param int $week
+     * @param int $day
+     * @return \DateTime
+     */
+    public function setISODate(int $year, int $week , int $day = 1): \DateTime
+    {
+        $this->flag_dirty();
+        return parent::setISODate($year, $week, $day);
+    }
 
-	public function setTime($hour, $minute, $second = 0, $microseconds = 0)
-	{
-		$this->flag_dirty();
-		return parent::setTime($hour, $minute, $second);
-	}
+    /**
+     * @param int $hour
+     * @param int $minute
+     * @param int $second
+     * @param int $microseconds
+     * @return \DateTime|false
+     */
+    public function setTime(int $hour, int $minute, int $second = 0, int $microseconds = 0): \DateTime
+    {
+        $this->flag_dirty();
+        return parent::setTime($hour, $minute, $second);
+    }
 
-	public function setTimestamp($unixtimestamp)
-	{
-		$this->flag_dirty();
-		return parent::setTimestamp($unixtimestamp);
-	}
+    /**
+     * @param int $unixtimestamp
+     * @return \DateTime
+     */
+    public function setTimestamp(int $unixtimestamp): \DateTime
+    {
+        $this->flag_dirty();
+        return parent::setTimestamp($unixtimestamp);
+    }
 
-	public function setTimezone($timezone)
-	{
-		$this->flag_dirty();
-		return parent::setTimezone($timezone);
-	}
-	
-	public function modify($modify)
-	{
-		$this->flag_dirty();
-		return parent::modify($modify);
-	}
-	
-	public function add($interval)
-	{
-		$this->flag_dirty();
-		return parent::add($interval);
-	}
+    /**
+     * @param \DateTimeZone $timezone
+     * @return \DateTime
+     */
+    public function setTimezone(\DateTimeZone $timezone): \DateTime
+    {
+        $this->flag_dirty();
+        return parent::setTimezone($timezone);
+    }
 
-	public function sub($interval)
-	{
-		$this->flag_dirty();
-		return parent::sub($interval);
-	}
+    /**
+     * @param string $modify
+     * @return \DateTime
+     */
+    public function modify(string $modify): \DateTime
+    {
+        $this->flag_dirty();
+        return parent::modify($modify);
+    }
+
+    /**
+     * @param \DateInterval $interval
+     * @return \DateTime
+     */
+    public function add(\DateInterval $interval): \DateTime
+    {
+        $this->flag_dirty();
+        return parent::add($interval);
+    }
+
+    /**
+     * @param \DateInterval $interval
+     * @return \DateTime
+     */
+    public function sub(\DateInterval $interval): \DateTime
+    {
+        $this->flag_dirty();
+        return parent::sub($interval);
+    }
 
 }
